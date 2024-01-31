d84410995f45101c4ea4291d3efe9b2d40c1c381
Wildan2024 <158287720+Wildan2024@users.noreply.github.com>
2024-02-01 02:35:14 +0700

    must_join.py

--- StringSessionBot/must_join.py
+++ StringSessionBot/must_join.py
@@ -14,20 +14,20 @@ async def must_join_channel(bot: Client, msg: Message):
             await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
         except UserNotParticipant:
             if MUST_JOIN.isalpha():
-                link = "https://t.me/" + MUST_JOIN
+                link = "https://t.me/Disney_storeDan" + MUST_JOIN
             else:
                 chat_info = await bot.get_chat(MUST_JOIN)
                 link = chat_info.invite_link
             try:
                 await msg.reply(
-                    f"𝙹𝙾𝙸𝙽 𝙺𝙴 𝙶𝚁𝚄𝙿 𝙳𝚄𝙻𝚄 [GROUP]({link}) 𝚄𝙽𝚃𝚄𝙺 𝙼𝙴𝙽𝙶𝙶𝚄𝙽𝙰𝙺𝙰𝙽 𝙱𝙾𝚃 𝙸𝙽𝙸. 𝚂𝙴𝚃𝙴𝙻𝙰𝙷 𝙸𝚃𝚄 𝚂𝚃𝙰𝚁𝚃 𝙺𝙴𝙼𝙱𝙰𝙻𝙸 /start",
+                    f"𝙹𝙾𝙸𝙽 𝙺𝙴 𝙶𝚁𝚄𝙿 𝙳𝚄𝙻𝚄 [GROUP]({Disney_storeDan}) 𝚄𝙽𝚃𝚄𝙺 𝙼𝙴𝙽𝙶𝙶𝚄𝙽𝙰𝙺𝙰𝙽 𝙱𝙾𝚃 𝙸𝙽𝙸. 𝚂𝙴𝚃𝙴𝙻𝙰𝙷 𝙸𝚃𝚄 𝚂𝚃𝙰𝚁𝚃 𝙺𝙴𝙼𝙱𝙰𝙻𝙸 /start",
                     disable_web_page_preview=True,
                     reply_markup=InlineKeyboardMarkup([
-                        [InlineKeyboardButton("ᴋᴀᴢᴜ sᴜᴘᴘᴏʀᴛ​", url=link)]
+                        [InlineKeyboardButton("ᴅᴀɴ sᴜᴘᴘᴏʀᴛ​", url=f"https://t.me/musicsupport_dan")]
                     ])
                 )
                 await msg.stop_propagation()
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"I'm not admin in the MUST_JOIN chat : {MUST_JOIN} !")
+        print(f"I'm not admin in the MUST_JOIN chat : {MUST_JOIN} https://t.me/Disney_storeDan")
