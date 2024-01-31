28e1b7d073b7f15511343ac94a50e7cadcc1837e
Wildan2024 <158287720+Wildan2024@users.noreply.github.com>
2024-02-01 02:46:15 +0700

    Config.py

--- Config.py
+++ Config.py
@@ -12,8 +12,8 @@ if ENVIRONMENT:
     DATABASE_URL = os.environ.get('DATABASE_URL', None)
     # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
     MUST_JOIN = os.environ.get('MUST_JOIN', None)
-    if MUST_JOIN.startswith("@"):
-        MUST_JOIN = MUST_JOIN.replace("@", "")
+    if MUST_JOIN.startswith("@Disney_storeDan"):
+        MUST_JOIN = MUST_JOIN.replace("@musicsupport_dan", "")
 else:
     # Fill the Values
     API_ID = 0
@@ -22,5 +22,5 @@ else:
     DATABASE_URL = ""
     DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
     MUST_JOIN = ""
-    if MUST_JOIN.startswith("@"):
+    if MUST_JOIN.startswith("@Disney_storeDan"):
         MUST_JOIN = MUST_JOIN[1:]
